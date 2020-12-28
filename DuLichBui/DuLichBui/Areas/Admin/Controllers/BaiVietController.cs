d8b7d1c24dd397c8d7cbe44a87b8718c410a8072
Duy Nguyen <vanduy290499@gmail.com>
2020-12-28 18:19:09 +0700

    Area Admin

---
+++ DuLichBui/DuLichBui/Areas/Admin/Controllers/BaiVietController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DuLichBui.Areas.Admin.Controllers
+{
+    public class BaiVietController : Controller
+    {
+        // GET: Admin/BaiViet
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
