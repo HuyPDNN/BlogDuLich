5468147b23490a5d14e8f43093fa954e50114044
Duy Nguyen <vanduy290499@gmail.com>
2020-12-29 19:03:04 +0700

    BaiVietDao

---
+++ DuLichBui/Model/Dao/BaiVietDao.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Model.EF;
+using PagedList;
+
+namespace Model.Dao
+{
+    public class BaiVietDao
+    {
+        DulichBuiDbContext db = null;
+        public BaiVietDao()
+        {
+            db = new DulichBuiDbContext();
+        }
+        public String ThemBaiViet(BaiViet entity)
+        {
+            db.BaiViet.Add(entity);
+            db.SaveChanges();
+            return entity.MaBaiViet;
+        }
+        public IEnumerable<BaiViet> DanhSachBaiViet(int page, int pageSize )
+        {
+            return db.BaiViet.OrderBy(o => o.NgayDang).ToPagedList(page, pageSize);
+        }
+        //public int TrangThaiBaiViet(string baiviet)
+        //{
+        //    var result = db.BaiViet.SingleOrDefault(o => o.MaBaiViet == baiviet);
+        //    if (result.TrangThai == "Lưu Nháp")
+        //    {
+        //        return 0;
+        //    }
+        //    else if (result.TrangThai == "Chờ Phê Duyệt")
+        //    {
+        //        return 1;
+        //    }
+        //    else if(result.TrangThai == "Đã Xuất Bản")
+        //    {
+        //        return 2;
+        //    }
+        //    else
+        //    {
+        //        return -1;
+        //    }
+        //}
+    }
+}
+
